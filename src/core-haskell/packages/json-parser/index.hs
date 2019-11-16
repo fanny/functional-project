c7685c3f084e09d27bd9e7cf0b67b2544e6ae5e6
ViniFarias <vinifarias.vf@gmail.com>
2019-11-16 13:24:22 -0300

    feat: add json-parser

---
+++ src/core-haskell/packages/json-parser/index.hs
@@ -0,0 +1,19 @@
+module JsonParser (
+  getTransactions
+) where
+
+import qualified Data.ByteString.Lazy as B
+import Data.Aeson
+import Transaction
+import Data.Maybe
+
+jsonFilePath :: FilePath
+jsonFilePath = "../../../data/transactions.json"
+
+getFile :: IO B.ByteString
+getFile = B.readFile jsonFilePath
+
+getTransactions :: IO [Transaction]
+getTransactions = do
+    transactions <- (decode <$> getFile) :: IO (Maybe [Transaction])
+    return (fromJust transactions)
