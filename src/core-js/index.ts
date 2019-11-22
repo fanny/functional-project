db1254aca13ff350137b9db55278ba5d719a46a1
fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-11-22 09:29:31 -0300

    Fix index

--- src/core-js/index.ts
+++ src/core-js/index.ts
@@ -1,26 +1,26 @@
 import {
-  getRevenueByPeriod,
-  getExpenseByPeriod,
-  getRemainByPeriod,
-  getTotalBalanceByPeriod,
-  getMaxBalanceByPeriod,
-  getMinBalanceByPeriod,
-  getAvgRevenuesByPeriod,
-  getAvgExpensesByPeriod,
-  getAvgRemainsByPeriod,
-  getCashFlow,
-} from './resolvers/queries'
-
-
-console.log(getRevenueByPeriod({year: 2019, month: 4}))
-console.log(getExpenseByPeriod({year: 2019, month: 4}))
-console.log(getRemainByPeriod({year: 2019, month: 4}))
-console.log(getTotalBalanceByPeriod({year: 2019, month: 4}))
-console.log(getMinBalanceByPeriod({year: 2019, month: 4}))
-console.log(getAvgRevenuesByPeriod({year: 2019}))
-console.log(getAvgExpensesByPeriod({year: 2019}))
-console.log(getAvgRemainsByPeriod({year: 2019}))
-console.log(getCashFlow({year: 2019, month: 4}))
-console.log(getMaxBalanceByPeriod({year: 2019, month: 4}))
-
-
+   filterByPeriod,
+   getRevenuesByPeriod,
+   getExpensesByPeriod,
+   getRemainsByPeriod,
+   getTotalBalanceByPeriod,
+   getMaxBalanceByPeriod,
+   getMinBalanceByPeriod,
+   getAvgRevenuesByPeriod,
+   getAvgExpensesByPeriod,
+   getAvgRemainsByPeriod,
+ } from './resolvers/queries'
+ 
+ 
+ export {
+   filterByPeriod,
+   getRevenuesByPeriod,
+   getExpensesByPeriod,
+   getRemainsByPeriod,
+   getTotalBalanceByPeriod,
+   getMaxBalanceByPeriod,
+   getMinBalanceByPeriod,
+   getAvgRevenuesByPeriod,
+   getAvgExpensesByPeriod,
+   getAvgRemainsByPeriod
+ }
