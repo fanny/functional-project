0159e97548a0c0bc6cd04428f11281acbf5a746a
ViniFarias <vinifarias.vf@gmail.com>
2019-11-22 23:44:26 -0300

    chore: add helper functions

--- src/core-haskell/resolvers/Helpers.hs
+++ src/core-haskell/resolvers/Helpers.hs
@@ -4,7 +4,7 @@ module Helpers (
   isRevenueOrExpense,
   checkYear,
   checkMonth,
-  average
+  mean
 ) where
 
 import GregorianCalendar
@@ -19,7 +19,7 @@ checkMonth :: Integer -> Transaction -> Bool
 checkMonth m t = month (date t) == m
 
 isRevenueOrExpense :: Transaction -> Bool
-isRevenueOrExpense t = ([VALOR_APLICACAO, APLICACAO] `intersect` (transactionTypes t)) == []
+isRevenueOrExpense t = ([VALOR_APLICACAO, APLICACAO, SALDO_CORRENTE] `intersect` (transactionTypes t)) == []
 
 isRevenue :: Transaction -> Bool
 isRevenue t = (value t) >= 0 && (isRevenueOrExpense t)
@@ -27,5 +27,21 @@ isRevenue t = (value t) >= 0 && (isRevenueOrExpense t)
 isExpense :: Transaction -> Bool
 isExpense t = (value t) < 0 && (isRevenueOrExpense t)
 
-average :: [Transaction] -> Float
-average transactions = sum (map (value) transactions) / fromIntegral (length transactions)
+mean :: [Transaction] -> Float
+mean transactions = sum (map (value) transactions) / fromIntegral (length transactions)
+
+-- Retorna uma lista contendo o saldo para cada dia da lista de transações.
+getDaysBalances :: [Transaction] -> [Float]
+getDaysBalances transactions = map (getBalance) (groupTransactionsByDay transactions)
+
+-- Retorna o saldo para a lista de transações.
+getBalance :: [Transaction] -> Float
+getBalance transactions = sum (map (value) transactions)
+
+-- Agrupa a lista de transações por dia.
+groupTransactionsByDay :: [Transaction] -> [[Transaction]]
+groupTransactionsByDay transactions = (groupBy ((==) `on` getDay) transactions)
+
+-- Retorna o dia do mês de uma transação.
+getDay :: Transaction -> Integer
+getDay t = dayOfMonth (date t)
