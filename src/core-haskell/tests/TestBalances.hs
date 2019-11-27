b1e16a123909823c36c57c1d35cc4a363667dee2
ViniFarias <vinifarias.vf@gmail.com>
2019-11-27 10:29:00 -0300

    test: refactor tests

--- src/core-haskell/tests/TestBalances.hs
+++ src/core-haskell/tests/TestBalances.hs
@@ -7,92 +7,38 @@ import JsonParser
 import Queries
 
 -- Testes referentes ao cálculo do saldo final em um determinado mês e ano.
-testFinalBalance01 = TestCase (
+testFinalBalance = TestCase (
     do
       db <- getTestTransactions
       assertEqual "Calcula o saldo final em Janeiro de 2018" 41812.6 (getFinalBalance db 2018 0)
+      assertEqual "Calcula o saldo final em Maio de 2018" 57822.57 (getFinalBalance db 2018 4)
+      assertEqual "Calcula o saldo final em Janeiro de 2019" 135209.42 (getFinalBalance db 2019 0)
+      assertEqual "Calcula o saldo final em Abril de 2019" 154276.18000000002 (getFinalBalance db 2019 3)
     )
 
-testFinalBalance02 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o saldo final em Maio de 2018" 57822.57 (getFinalBalance db 2018 4)
-  )
-
-testFinalBalance03 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o saldo final em Janeiro de 2019" 135209.42 (getFinalBalance db 2019 0)
-  )
-
-testFinalBalance04 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o saldo final em Abril de 2019" 154276.18000000002 (getFinalBalance db 2019 3)
-  )
-
 -- Testes referentes ao cálculo do saldo máximo em um determinado mês e ano.
-testMaxBalance01 = TestCase (
+testMaxBalance = TestCase (
     do
       db <- getTestTransactions
       assertEqual "Calcula o saldo máximo em Janeiro de 2018" 41812.6 (getMaxBalance db 2018 0)
+      assertEqual "Calcula o saldo máximo em Maio de 2018" 57822.57 (getMaxBalance db 2018 4)
+      assertEqual "Calcula o saldo máximo em Janeiro de 2019" 135209.42 (getMaxBalance db 2019 0)
+      assertEqual "Calcula o saldo máximo em Abril de 2019" 154276.18000000002 (getMaxBalance db 2019 3)
     )
 
-testMaxBalance02 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o saldo máximo em Maio de 2018" 57822.57 (getMaxBalance db 2018 4)
-  )
-
-testMaxBalance03 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o saldo máximo em Janeiro de 2019" 135209.42 (getMaxBalance db 2019 0)
-  )
-
-testMaxBalance04 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o saldo máximo em Abril de 2019" 154276.18000000002 (getMaxBalance db 2019 3)
-  )
-
 -- Testes referentes ao cálculo do saldo mínimo em um determinado mês e ano.
-testMinBalance01 = TestCase (
+testMinBalance = TestCase (
     do
       db <- getTestTransactions
       assertEqual "Calcula o saldo mínimo em Janeiro de 2018" 41812.6 (getMinBalance db 2018 0)
+      assertEqual "Calcula o saldo mínimo em Maio de 2018" 57822.57 (getMinBalance db 2018 4)
+      assertEqual "Calcula o saldo mínimo em Janeiro de 2019" 130801.66 (getMinBalance db 2019 0)
+      assertEqual "Calcula o saldo mínimo em Abril de 2019" 153797.14 (getMinBalance db 2019 3)
     )
-
-testMinBalance02 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o saldo mínimo em Maio de 2018" 57822.57 (getMinBalance db 2018 4)
-  )
-
-testMinBalance03 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o saldo mínimo em Janeiro de 2019" 130801.66 (getMinBalance db 2019 0)
-  )
-
-testMinBalance04 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o saldo mínimo em Abril de 2019" 153797.14 (getMinBalance db 2019 3)
-  )
 
 testBalances = 
   TestList [
-    testFinalBalance01,
-    testFinalBalance02,
-    testFinalBalance03,
-    testFinalBalance04,
-    testMaxBalance01,
-    testMaxBalance02,
-    testMaxBalance03,
-    testMaxBalance04,
-    testMinBalance01,
-    testMinBalance02,
-    testMinBalance03,
-    testMinBalance04
+    testFinalBalance,
+    testMaxBalance,
+    testMinBalance
   ]
--- src/core-haskell/tests/TestValues.hs
+++ src/core-haskell/tests/TestValues.hs
@@ -7,92 +7,38 @@ import JsonParser
 import Queries
 
 -- Testes referentes ao cálculo do valor da receita em um determinado mês e ano.
-testRevenueValue01 = TestCase (
+testRevenueValue = TestCase (
     do
       db <- getTestTransactions
       assertEqual "Calcula o valor da receita em Janeiro de 2018" 92.45 (getRevenueValue db 2018 0)
+      assertEqual "Calcula o valor da receita em Maio de 2018" 929.44 (getRevenueValue db 2018 4)
+      assertEqual "Calcula o valor da receita em Janeiro de 2019" 4407.76 (getRevenueValue db 2019 0)
+      assertEqual "Calcula o valor da receita em Abril de 2019" 479.04 (getRevenueValue db 2019 3)
     )
 
-testRevenueValue02 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o valor da receita em Maio de 2018" 929.44 (getRevenueValue db 2018 4)
-  )
-
-testRevenueValue03 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o valor da receita em Janeiro de 2019" 4407.76 (getRevenueValue db 2019 0)
-  )
-
-testRevenueValue04 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o valor da receita em Abril de 2019" 479.04 (getRevenueValue db 2019 3)
-  )
-
 -- Testes referentes ao cálculo do valor da despesa em um determinado mês e ano.
-testExpenseValue01 = TestCase (
+testExpenseValue = TestCase (
     do
       db <- getTestTransactions
       assertEqual "Calcula o valor da despesas em Janeiro de 2018" 2260.0 (getExpenseValue db 2018 0)
+      assertEqual "Calcula o valor da despesas em Maio de 2018" 250.0 (getExpenseValue db 2018 4)
+      assertEqual "Calcula o valor da despesas em Janeiro de 2019" 500.0 (getExpenseValue db 2019 0)
+      assertEqual "Calcula o valor da despesas em Abril de 2019" 10000.0 (getExpenseValue db 2019 3)
     )
 
-testExpenseValue02 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o valor da despesas em Maio de 2018" 250.0 (getExpenseValue db 2018 4)
-  )
-
-testExpenseValue03 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o valor da despesas em Janeiro de 2019" 500.0 (getExpenseValue db 2019 0)
-  )
-
-testExpenseValue04 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o valor da despesas em Abril de 2019" 10000.0 (getExpenseValue db 2019 3)
-  )
-
 -- Testes referentes ao cálculo do valor da sobra em um determinado mês e ano.
-testRemainsValue01 = TestCase (
+testRemainsValue = TestCase (
     do
       db <- getTestTransactions
       assertEqual "Calcula o valor da sobra em Janeiro de 2018" (-2167.55) (getRemainsValue db 2018 0)
+      assertEqual "Calcula o valor da sobra em Maio de 2018" 679.44 (getRemainsValue db 2018 4)
+      assertEqual "Calcula o valor da sobra em Janeiro de 2019" 3907.76 (getRemainsValue db 2019 0)
+      assertEqual "Calcula o valor da sobra em Abril de 2019" (-9520.96) (getRemainsValue db 2019 3)
     )
-
-testRemainsValue02 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o valor da sobra em Maio de 2018" 679.44 (getRemainsValue db 2018 4)
-  )
-
-testRemainsValue03 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o valor da sobra em Janeiro de 2019" 3907.76 (getRemainsValue db 2019 0)
-  )
-
-testRemainsValue04 = TestCase (
-  do
-    db <- getTestTransactions
-    assertEqual "Calcula o valor da sobra em Abril de 2019" (-9520.96) (getRemainsValue db 2019 3)
-  )
 
 testValues = 
   TestList [
-    testExpenseValue01,
-    testExpenseValue02,
-    testExpenseValue03,
-    testExpenseValue04,
-    testRevenueValue01,
-    testRevenueValue02,
-    testRevenueValue03,
-    testRevenueValue04,
-    testRemainsValue01,
-    testRemainsValue02,
-    testRemainsValue03,
-    testRemainsValue04
+    testExpenseValue,
+    testRevenueValue,
+    testRemainsValue
   ]
